46aa6c1026818af66e76bfe1ba15dca984bf0bd7
Michael Chambers <mchambers175@gmail.com>
2019-04-12 15:35:13 -0700

    added temporary static code to pug templates

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
+var sandboxRouter = require('./routes/sandbox');
 
 var app = express();
 
@@ -20,7 +21,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/sandbox', sandboxRouter);
+app.use('/books', booksRouter);
+
+// app.get('/books', function(req, res, next) {
+//   res.render('books', {prompt: "I'm a variable"});
+// });
+
+// app.get('/sandbox', (req, res, next) => {
+//   res.render('sandbox', {names});
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
