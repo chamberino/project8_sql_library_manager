cb82397812a6a611205eb701d9c3afa1f1ac7b60
Michael Chambers <mchambers175@gmail.com>
2019-04-13 11:27:29 -0700

    Added book creation to library db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -23,14 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/sandbox', sandboxRouter);
 app.use('/books', booksRouter);
-
-// app.get('/books', function(req, res, next) {
-//   res.render('books', {prompt: "I'm a variable"});
-// });
-
-// app.get('/sandbox', (req, res, next) => {
-//   res.render('sandbox', {names});
-// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/books.js
+++ routes/books.js
@@ -2,9 +2,55 @@ var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
 
-/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.render('books/test');
+// });
+//
+// router.get('/', function(req, res, next) {
+//   res.render('books/books');
+// });
+
+var books = [
+  {
+    id: 1,
+    title: "Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Fiction",
+    year: 1927,
+  },
+];
+
+/* GET books listing. */
 router.get('/', function(req, res, next) {
-  res.render('books');
+  Book.findAll({order: [["createdAt", "DESC"]]}).then(function(books){
+    res.render("books/index", {books: books, title: "Library DB" });
+  });
+});
+
+// render books template
+router.get('/', function(req, res, next) {
+  res.render('books/booksTemp', {books});
+});
+
+
+/* POST create book. */
+router.post('/', function(req, res, next) {
+  Book.create(req.body).then(function(book) {
+    res.redirect("/books/" + book.id);
+  });
+;});
+
+
+/* Create a new book form. */
+router.get('/new', function(req, res, next) {
+  res.render("books/new", {book: Book.build(), title: "New Book"});
+});
+
+/* GET individual article. */
+router.get("/:id", function(req, res, next){
+  Book.findByPk(req.params.id).then(function(book){
+    res.render("books/show", {book: book, title: book.title});
+  });
 });
 
 module.exports = router;
